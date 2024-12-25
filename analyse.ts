44885314531793b540fbbf620b446787b56c4266
Mael <laem@kont.me>
2024-12-25 21:36:30 +0100

    Ici on reteste scrape avec nitter poast, mais poast tombe tout le temps
    Enfin nous bannit tout le temps

--- analyse.ts
+++ analyse.ts
@@ -2,41 +2,85 @@ import { scrape } from 'jsr:@panha/scrape/'
 import { parse, stringify } from 'jsr:@std/csv'
 const delay = (ms) => new Promise((res) => setTimeout(res, ms))
 
+const alreadyDone = JSON.parse(Deno.readTextFileSync('data.json') || '{}')
+
+const doneEntries = Object.entries(alreadyDone)
+console.log(`Déjà ${doneEntries.length} comptes de députés vérifiés`)
+
 import { launch } from 'jsr:@astral/astral'
 
 const csv = Deno.readTextFileSync('députés-datan-25-12-2024.csv')
 
-const députés = parse(csv, {
+const députésRaw = parse(csv, {
   skipFirstRow: true,
   strip: true,
 })
 
-const hasTwitter = députés.filter((d) => d.twitter && d.twitter !== '').length,
-  hasNot = députés.length - hasTwitter
+const députés = députésRaw.map((d) => {
+  if (!d.twitter) return d
+  const match = d.twitter.match(/x\.com\/(.+)$/)
+  if (match) return { ...d, twitter: '@' + match[1] }
+  return d
+})
 
-console.log(hasTwitter, hasNot, députés.length)
+const hasTwitter = députés.filter((d) => d.twitter && d.twitter !== ''),
+  hasNotLength = députés.length - hasTwitter.length
 
-const bridés = députés.slice(0, 2)
+console.log(hasTwitter.length, hasNotLength, députés.length)
 
-const atList = [...bridés.map((d) => d.twitter), '@ALeaument']
+const bridés = hasTwitter
+  .filter((d) => !doneEntries.find(([at]) => at === d.twitter))
+  .slice(0, 10)
+
+const atList = [...bridés.map((d) => d.twitter)]
 
 const doFetch = async () => {
-  const entries = await Promise.all(atList.map(checkAt))
+  const entries = await Promise.all(
+    atList.map((at, i) => checkAt(at, i * 12000))
+  )
   const o = Object.fromEntries(entries)
-  Deno.writeTextFileSync('./data.json', JSON.stringify(o))
+  Deno.writeTextFileSync(
+    './data.json',
+    JSON.stringify({ ...alreadyDone, ...o })
+  )
   return console.log("Voilà c'est analysé dans ./data.json")
 }
 
-const checkAt = async (at) => {
+/* 
+const ws =
+  'ws://127.0.0.1:1337/devtools/browser/e82185e6-f90d-4da1-9a67-0a8445f82b85'
+
+const browser = await launch({
+  wsEndpoint: ws,
+  headless: false,
+})
+await delay(25000 / 10)
+*/
+
+const checkAt = async (at, ms) => {
+  await delay(ms)
   if (!at.startsWith('@') && at.length < 2)
-    throw new Error('Problème dans le pseudo ' + at)
+    throw new Error('Problème dans le pseudo ' + at + '.')
 
   const netAt = at.slice(1)
   console.log('Lancement du scraping pour ', netAt)
 
   //const url = 'https://xcancel.com/' + netAt
-  const url = 'https://x.com/' + netAt
+  //const url = 'https://x.com/' + netAt
+  const url = 'https://nitter.poast.org/' + netAt
+  //const url = 'https://cartes.app/blog'
   console.log('will' + url)
+
+  /* 
+  const page = await browser.newPage(url)
+  await delay(2000)
+  // Run code in the context of the browser
+  const value = await page.locator('small').evaluateAll((el) => el.textContent)
+  console.log('sq', value)
+
+  // Close connection
+  //await browser.close()
+
   /*
   const browser = await launch()
   const page = await browser.newPage(url)
@@ -48,6 +92,7 @@ const checkAt = async (at) => {
   Deno.writeFileSync('./screenshot' + at + '.png', screenshot)
   console.log('written')
   */
+
   let scraper = null
 
   try {
@@ -63,13 +108,20 @@ const checkAt = async (at) => {
       `<html>${scraper.html('html')}</html`
     )
 
-    const result = scraper.attr('title', '.tweet-date')
-    console.log(result)
+    const innerHTMLList = scraper.html('.tweet-date')
+    if (innerHTMLList.length < 3)
+      throw new Error(
+        "Pas assez de tweets trouvés, c'est suspect ! Investiguer " + at
+      )
+    console.log('inner', innerHTMLList)
+    const dates = innerHTMLList.map(dateFromAttr)
+    /* 
     //"2024-12-24T10:52:26.000Z"
     const dates = result.map((date) => new Date(date))
+	*/
 
     const nowStamp = new Date().getTime()
-    const threeDaysSpan = 24 * 60 * 60 * 1000
+    const threeDaysSpan = 3 * 24 * 60 * 60 * 1000
 
     const recentTweets = dates.map(
       (date) => date.getTime() > nowStamp - threeDaysSpan
@@ -79,7 +131,41 @@ const checkAt = async (at) => {
     return [at, hasRecentTweets]
   } catch (e) {
     if (!scraper) return console.log(e, 'Problème de scrap') || [at, 'error']
-    console.log(scraper.html('body'))
+    console.log('Voir le fichier HTML créé pour ' + at)
+  }
+}
+
+//<a href="/JeromeGuedj/status/1630575199975243776#m" title="Feb 28, 2023 · 2:26 PM UTC">28 Feb 2023</a>
+const regex = /title="([A-Z][a-z][a-z]) (\d+), (\d\d\d\d)/
+const monthNames = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+]
+const dateFromAttr = (str) => {
+  const match = str.match(regex)
+
+  if (match) {
+    const month = match[1]
+    const day = match[2]
+    const year = match[3]
+    //    console.log(`Day: ${day}, Month: ${month}, Year: ${year}`)
+
+    const monthIndex = monthNames.indexOf(month)
+    const date = new Date(year, monthIndex, day)
+    return date
+  } else {
+    console.log('No match found')
+    return null
   }
 }
 
