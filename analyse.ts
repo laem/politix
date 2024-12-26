5fced2548f7c70eb1f5a9250cc669d8b548c446a
Mael <laem@kont.me>
2024-12-26 19:41:39 +0100

    Re-nouveaux

--- analyse.ts
+++ analyse.ts
@@ -49,7 +49,7 @@ const bridés = hasTwitter
         doneEntries.find(([at]) => at === d.twitter)
       )
   )
-  .slice(0, 60)
+  .slice(0, 100)
 
 const atList = [...bridés.map((d) => d.twitter)]
 
