53f56722532295994f6c71f430a03753f59d7acf
Mael <laem@kont.me>
2024-12-27 09:33:01 +0100

    Titre

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,10 +1,7 @@
-import { useSignal } from '@preact/signals'
-import Counter from '../islands/Counter.tsx'
 import Results from '../components/Results.tsx'
 import { daysSpan } from '../date-utils.ts'
 
 export default function Home() {
-  const count = useSignal(3)
   return (
     <div class="px-4 py-8 mx-auto ">
       <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
