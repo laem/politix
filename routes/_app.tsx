5ce46a71c24151d395c9c611346e8188181e9378
Mael <laem@kont.me>
2024-12-28 21:16:55 +0100

    Nouveau titre

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,7 +5,10 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Politix - Les politiciens encore actifs sur X (Twitter)</title>
+        <title>
+          Politix - Répartition par parti des députés actifs sur X (Twitter) et
+          Bluesky
+        </title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
