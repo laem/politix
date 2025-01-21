488d4bbb08b322a5af7f7cc36da364a45f0c7f6f
Mael <laem@kont.me>
2025-01-21 22:01:29 +0100

    MAJ auth pour bluesky top, ils arrêtent le public; maj du jour

--- topBlueskyFr.ts
+++ topBlueskyFr.ts
@@ -1,27 +1,51 @@
-import { delay } from "./utils.ts"
+import { AtpAgent, AtpSessionEvent, AtpSessionData } from 'npm:@atproto/api'
+import { delay } from './utils.ts'
 
 const falsePositives = [
-  "labordeliere.bsky.social", // +18 ans, ne skeete que des images donc pas francophone
-  "antifapuddinpop.bsky.social", // marked as french but not https://bsky.app/profile/antifapuddinpop.bsky.social/post/3lffmbgxick2x
+  'labordeliere.bsky.social', // +18 ans, ne skeete que des images donc pas francophone
+  'antifapuddinpop.bsky.social', // marked as french but not https://bsky.app/profile/antifapuddinpop.bsky.social/post/3lffmbgxick2x
 ]
 
 let old
 
 try {
-  old = JSON.parse(Deno.readTextFileSync("bluesky-top-actors-fr.json") || "{}")
+  old = JSON.parse(Deno.readTextFileSync('bluesky-top-actors-fr.json') || '{}')
 } catch (e) {
   old = { sorted: {}, dates: [] }
 }
 
+const agent = new AtpAgent({
+  service: 'https://bsky.social/xrpc',
+  persistSession: (evt: AtpSessionEvent, sess?: AtpSessionData) => {
+    // store the session-data for reuse
+  },
+})
+
 const analyse = async () => {
+  await agent.login({
+    identifier: 'mael.kont.me',
+    password: 'wvkd-edeu-wjnt-yf5j',
+  })
+
+  console.log('OK', agent.did)
+
+  const { data: json } = await agent.app.bsky.feed.searchPosts({
+    q: 'lang:fr',
+    limit: 100,
+    sort: 'top',
+  })
+
+  /* public API not working anymore 21 jan
   const url =
-    "https://public.api.bsky.app/xrpc/app.bsky.feed.searchPosts?q=lang:fr&limit=100&sort=top"
+    'https://public.api.bsky.app/xrpc/app.bsky.feed.searchPosts?q=lang:fr&limit=100&sort=top'
 
   const request = await fetch(url)
   const json = await request.json()
 
+		*/
+
   const dates = new Set(
-    json.posts.map((post) => post.record.createdAt.split("T")[0]),
+    json.posts.map((post) => post.record.createdAt.split('T')[0])
   )
 
   const handles = new Set([
@@ -33,7 +57,7 @@ const analyse = async () => {
     [...handles].map(async (handle, i) => {
       await delay(i * 100)
       const request = await fetch(
-        `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${handle}`,
+        `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${handle}`
       )
       const json = await request.json()
 
@@ -41,7 +65,7 @@ const analyse = async () => {
 
       const result = [handle, count]
       return result
-    }),
+    })
   )
 
   const totalEntries = {
@@ -52,7 +76,7 @@ const analyse = async () => {
   const sorted = Object.fromEntries(
     Object.entries(totalEntries)
       .sort(([, a], [, b]) => b - a)
-      .filter(([handle]) => !falsePositives.includes(handle)),
+      .filter(([handle]) => !falsePositives.includes(handle))
   )
 
   /*
@@ -71,11 +95,11 @@ const analyse = async () => {
   }
 
   Deno.writeTextFileSync(
-    "./bluesky-top-actors-fr.json",
-    JSON.stringify(result, null, 2),
+    './bluesky-top-actors-fr.json',
+    JSON.stringify(result, null, 2)
   )
 
-  console.log("Fichier écrit")
+  console.log('Fichier écrit')
 }
 
 analyse()
