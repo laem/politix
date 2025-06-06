7095577f0bb86ac8937feab71d355b15df9652d5
Viango <viango@tutamail.com>
2025-06-06 21:22:39 +0200

    Mise à jour des données

--- analyse_data.sh
+++ analyse_data.sh
@@ -4,25 +4,25 @@ basic_allow="--allow-read --allow-net"
 env_allow="--allow-env --env-file"
 
 # Bluesky
-# deno run ${basic_allow} --allow-write=data/bluesky-data.json analyse/analyseBluesky.ts
+deno run ${basic_allow} --allow-write=data/bluesky-data.json analyse/analyseBluesky.ts
 
 # Mastodon
-# deno run ${basic_allow} ${env_allow} --allow-write=data/mastodon-data.json analyse/analyseMastodon.ts
+deno run ${basic_allow} ${env_allow} --allow-write=data/mastodon-data.json analyse/analyseMastodon.ts
 
 # Parlement Européen
-# deno run ${basic_allow} ${env_allow} --allow-write=data/europe-data.json analyse/analyseEurope.ts
+deno run ${basic_allow} ${env_allow} --allow-write=data/europe-data.json analyse/analyseEurope.ts
 
 
 # Début de l'analyse des comptes sur X
-rm -rf $HOME/.cache/astral
+# rm -rf $HOME/.cache/astral
 
 # Parlementaires français
-deno run ${basic_allow} --allow-env --allow-write=data/x-data.json,/tmp,$HOME/.cache/astral --allow-run analyse/analyse.ts
+# deno run ${basic_allow} --allow-env --allow-write=data/x-data.json,/tmp,$HOME/.cache/astral --allow-run analyse/analyse.ts
 
-rm -rf $HOME/.cache/astral
+# rm -rf $HOME/.cache/astral
 
 # Gouvernement français
-deno run ${basic_allow} --allow-env --allow-write=data/ministres.json,/tmp,$HOME/.cache/astral --allow-run analyse/analyseMinistres.ts
+# deno run ${basic_allow} --allow-env --allow-write=data/ministres.json,/tmp,$HOME/.cache/astral --allow-run analyse/analyseMinistres.ts
 
 # Fin de l'analyse des comptes sur X
 rm -rf $HOME/.cache/astral
