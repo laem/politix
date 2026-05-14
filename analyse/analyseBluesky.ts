e59b9aada5992cdad0bff0f25e5f532bf2ecb1ed
Mael <laem@kont.me>
2026-05-14 13:27:43 +0200

    Faux positif Gonzales

--- analyse/analyseBluesky.ts
+++ analyse/analyseBluesky.ts
@@ -1,22 +1,23 @@
-import députésRandomOrder from "../députés.ts"
-import { findBlueskyAccount, logResultBluesky } from "./findBlueskyAccount.ts"
+import députésRandomOrder from '../députés.ts'
+import { findBlueskyAccount, logResultBluesky } from './findBlueskyAccount.ts'
 
 const falsePositives = {
-  PA841825: ["patricemartin50.bsky.social"],
-  PA793262: ["onesque.bsky.social"],
+  PA841825: ['patricemartin50.bsky.social'],
+  PA793262: ['onesque.bsky.social'],
   PA793362: [
-    "williamjo.se",
-    "jose-eduardo.bsky.social", // need to be connected to be seen
+    'williamjo.se',
+    'jose-eduardo.bsky.social', // need to be connected to be seen
+    'jrgzz.bsky.social',
   ],
-  PA720614: ["mlpcdn.bsky.social", "mlp-jail.bsky.social"],
-  PA817203: ["lauremiller.bsky.social"],
-  PA793102: ["tristanhylare.bsky.social"],
-  PA841833: ["davidguerin.bsky.social"],
-  PA842255: ["cbourgonsicard.bsky.social"],
-  PA794886: ["sceauphie.bsky.social"],
+  PA720614: ['mlpcdn.bsky.social', 'mlp-jail.bsky.social'],
+  PA817203: ['lauremiller.bsky.social'],
+  PA793102: ['tristanhylare.bsky.social'],
+  PA841833: ['davidguerin.bsky.social'],
+  PA842255: ['cbourgonsicard.bsky.social'],
+  PA794886: ['sceauphie.bsky.social'],
 }
 
-const analyseDate = new Date().toISOString().split("T")[0]
+const analyseDate = new Date().toISOString().split('T')[0]
 const analyseBluesky = async () => {
   const extract = députésRandomOrder
   const results = await Promise.all(
@@ -44,10 +45,7 @@ const analyseBluesky = async () => {
   })
 
   const o = Object.fromEntries(entries)
-  Deno.writeTextFileSync(
-    "data/bluesky-data.json",
-    JSON.stringify(o, null, 2),
-  )
+  Deno.writeTextFileSync('data/bluesky-data.json', JSON.stringify(o, null, 2))
 }
 
 analyseBluesky()
