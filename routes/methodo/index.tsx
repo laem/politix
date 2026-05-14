eaf4665e310cb78d01c9e0972760f5506b7092ae
Mael <laem@kont.me>
2026-05-14 13:34:58 +0200

    On cite la source pour les députés fr, datan

--- routes/methodo/index.tsx
+++ routes/methodo/index.tsx
@@ -1,31 +1,26 @@
-import { Head } from "$fresh/runtime.ts"
-import BackToHome from "../../components/BackToHome.tsx"
+import { Head } from '$fresh/runtime.ts'
+import BackToHome from '../../components/BackToHome.tsx'
 
-const title = "Méthodologie"
-const description =
-  `Description de la méthodologie utilisée pour recueillir les données concernant les réseaux sociaux où sont présents les politiciens.`
+const title = 'Méthodologie'
+const description = `Description de la méthodologie utilisée pour recueillir les données concernant les réseaux sociaux où sont présents les politiciens.`
 
 export default function Top() {
   return (
     <main
       style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-      <div style={{ maxWidth: "50rem" }}>
+      <div style={{ maxWidth: '50rem' }}>
         <Head>
           <title>{title}</title>
-          <meta
-            name="description"
-            content={description}
-            key="description"
-          />
+          <meta name="description" content={description} key="description" />
         </Head>
         <BackToHome />
         <header
-          style={{ display: "flex", alignItems: "center", marginTop: "1rem" }}
+          style={{ display: 'flex', alignItems: 'center', marginTop: '1rem' }}
         >
           <span class="emoji">&#x1F4DD;</span>
           <h1>{title}</h1>
@@ -34,20 +29,23 @@ export default function Top() {
           Est considéré comme actif un politicien si il a envoyé un message dans
           les 7 derniers jours.
         </p>
-        <p>
-        </p>
+        <p></p>
         <p>
           Pour Bluesky et Mastodon, nous considérons que le premier résultat
           retourné par l'API est le compte du politicien recherché. Pour
-          Mastodon, nous utilisons le serveur{" "}
+          Mastodon, nous utilisons le serveur{' '}
           <a href="https://piaille.fr">piaille.fr</a> .
         </p>
         <br />
-        Les données viennent de <a href="https://datan.fr">datan.fr</a>{" "}
-        pour les députés français et du{" "}
+        Les données viennent de <a href="https://datan.fr">datan.fr</a> pour les
+        députés français via{' '}
+        <a href="https://www.data.gouv.fr/datasets/deputes-actifs-de-lassemblee-nationale-informations-et-statistiques">
+          ce jeu de données data.gouv
+        </a>
+        et du{' '}
         <a href="https://www.europarl.europa.eu/meps/fr/full-list/all">
           site officel du parlement européen
-        </a>{" "}
+        </a>{' '}
         pour les eurodéputés.
       </div>
     </main>
